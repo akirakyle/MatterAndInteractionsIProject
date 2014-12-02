fde65b108ede28c6b52ea080b7f9e24a77bb7d75
akyle9 <akyle@andrew.cmu.edu>
2014-12-02 14:15:53 -0500

    Worked on GasSim

--- GasSim.c
+++ GasSim.c
@@ -1,5 +1,5 @@
 //
-//  SpeedTest.c
+//  GasSim.c
 //  
 //
 //  Created by Akira Kyle on 10/26/14.
@@ -13,7 +13,7 @@
 
 int main (void) {
     clock_t start = clock(), diff;
-    FILE *file = fopen("Developer/MatterAndInteractionsIProject/gasData.txt", "w");
+    FILE *file = fopen("Developer/MatterAndInteractionsIProject/gasSimData.txt", "w");
     if (file == NULL)
     {
         printf("Error opening file!\n");
@@ -36,7 +36,7 @@ int main (void) {
         struct velelocity vel;
     };
     
-    int time = 2;
+    int time = 5;
     int iteration = 0;
     unsigned long long int num = 1000;
     float dt = 0.01;
@@ -44,28 +44,31 @@ int main (void) {
     struct particleStruct particle[num];
     
     for (int n=0; n<num; n++) {
-        particle[n].pos.x = (rand() % 20) - 10;
-        particle[n].pos.y = (rand() % 20) - 10;
-        particle[n].pos.z = (rand() % 20) - 10;
-        particle[n].vel.x = (rand() % 20) - 10;
-        particle[n].vel.y = (rand() % 20) - 10;
-        particle[n].vel.z = (rand() % 20) - 10;
+        particle[n].pos.x = ((float)rand()/(float)RAND_MAX * 18) - 9;
+        particle[n].pos.y = ((float)rand()/(float)RAND_MAX * 5) - 2.5;
+        particle[n].pos.z = ((float)rand()/(float)RAND_MAX * 5) - 2.5;
+        particle[n].vel.x = ((float)rand()/(float)RAND_MAX * 10) - 5;
+        particle[n].vel.y = ((float)rand()/(float)RAND_MAX * 10) - 5;
+        particle[n].vel.z = ((float)rand()/(float)RAND_MAX * 10) - 5;
+        //printf("pos:%f, vel:%f\n",particle[n].pos.x,particle[n].vel.x);
     }
     
     for (float t=0; t<time; t=t+dt) {
         for (unsigned long long int i=0; i<num; i++) {
-            if (particle[i].pos.x > 10 || particle[i].pos.x < -10) {
-                particle[i].vel.x = -particle[i].vel.x;
-            }
-            if (particle[i].pos.y > 10 || particle[i].pos.y < -10) {
+            if (sqrt(pow((particle[i].pos.y), 2) + pow((particle[i].pos.z), 2)) > 5) {
                 particle[i].vel.y = -particle[i].vel.y;
-            }
-            if (particle[i].pos.z > 10 || particle[i].pos.z < -10) {
                 particle[i].vel.z = -particle[i].vel.z;
             }
+            if (particle[i].pos.x > 10-t*2) {
+                particle[i].vel.x = -abs(particle[i].vel.x);
+            }
+            if (particle[i].pos.x < -10) {
+                particle[i].vel.x = abs(particle[i].vel.x);
+            }
             
-            for (unsigned long long int j=i; j<num; j++) {
-                if (sqrt(pow((particle[i].pos.x - particle[j].pos.x), 2) + pow((particle[i].pos.y - particle[j].pos.y), 2) + pow((particle[i].pos.z - particle[j].pos.z), 2)) < 1) {
+            for (unsigned long long int j=i+1; j<num; j++) {
+                if (sqrt(pow((particle[i].pos.x - particle[j].pos.x), 2) + pow((particle[i].pos.y - particle[j].pos.y), 2) + pow((particle[i].pos.z - particle[j].pos.z), 2)) < .2) {
+                    //printf("Distance from particle %llu to %llu: %f\n",i,j,sqrt(pow((particle[i].pos.x - particle[j].pos.x), 2) + pow((particle[i].pos.y - particle[j].pos.y), 2) + pow((particle[i].pos.z - particle[j].pos.z), 2)));
                     particle[i].vel.x = -particle[i].vel.x;
                     particle[i].vel.y = -particle[i].vel.y;
                     particle[i].vel.z = -particle[i].vel.z;
@@ -76,11 +79,9 @@ int main (void) {
             particle[i].pos.y = particle[i].pos.y + particle[i].vel.y*dt;
             particle[i].pos.z = particle[i].pos.z + particle[i].vel.z*dt;
             
-            if ((iteration % 20) == 0) {
-                fprintf(file, "%g: <%g,%g,%g>\n",t,particle[i].pos.x,particle[i].pos.y,particle[i].pos.z);
-            }
+            fprintf(file, "%f,%f,%f,%f\n",t,particle[i].pos.x,particle[i].pos.y,particle[i].pos.z);
         }
-        if ((iteration % 20) == 0) {
+        if ((iteration % 50) == 0) {
             printf("interation:%d\n",iteration);
             printf("time:%g\n",t);
             
@@ -93,6 +94,6 @@ int main (void) {
     diff = clock() - start;
     int msec = diff * 1000 / CLOCKS_PER_SEC;
     printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
-
+    
     return 0;
 }
--- GasSimVisualize2.py
+++ GasSimVisualize2.py
@@ -1,7 +1,8 @@
 from visual import *
 from random import randrange
 
-data = open("/Users/akyle/Developer/MatterAndInteractionsIProject/cData.txt","r")
+data = open("/Users/akyle/Developer/MatterAndInteractionsIProject/gasSimData.txt","r")
+scene.autoscale = False
 
 vscale = 0.1
 particle = []
@@ -9,19 +10,23 @@ pos = 0
 prevLineTime = 0
 diff = True
 lineNotNull = True
+first = True
 
 while diff:
     currentLine = data.readline()
     currentLineData = currentLine.split(",")
     for i in xrange(0,4):
         currentLineData[i] = float(currentLineData[i])
-    particle.append(sphere(pos=(currentLineData[1],currentLineData[2],currentLineData[3]), radius=0.5, color=color.red))
+    if first:
+        particle.append(sphere(pos=(currentLineData[1],currentLineData[2],currentLineData[3]), radius=0.2, color=color.blue))
+        first = False
+    particle.append(sphere(pos=(currentLineData[1],currentLineData[2],currentLineData[3]), radius=0.2, color=color.red))
     if prevLineTime != currentLineData[0]:
         diff = False
     prevLineTime = currentLineData[0]
 
 while lineNotNull:
-    rate(100)
+    rate(50)
     pos = 0
     diff = True
     while diff:
